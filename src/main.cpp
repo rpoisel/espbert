f64175f23e3c041114ce0f21578e7c338c838377
Rainer Poisel <rainer.poisel@gmail.com>
2018-12-26 19:57:02 +0100

    Internet Radio implemented with FreeRTOS tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,167 @@
-#include "SampleMp3.h"
+/**
+  A simple stream handler to play web radio stations using ESP8266
 
-#include <SPI.h>
-#if 0
-#include <MFRC522.h>
-#include <SdFat.h>
-#else
+  Copyright (C) 2018 Vince Gellár (github.com/vincegellar)
+  Licensed under GNU GPL v3
+
+  Wiring:
+  --------------------------------
+  | VS1053  | ESP8266 |  Other   |
+  --------------------------------
+  |   SCK   |   D5    |    -     |
+  |   MISO  |   D6    |    -     |
+  |   MOSI  |   D7    |    -     |
+  |   XRST  |    -    |    -     |
+  |   CS    |   D1    |    -     |
+  |   DCS   |   D0    |    -     |
+  |   DREQ  |   D3    |    -     |
+  |   5V    |    -    |   VCC    |
+  |   GND   |    -    |   GND    |
+  --------------------------------
+
+  Dependencies:
+  -VS1053 library by baldram (https://github.com/baldram/ESP_VS1053_Library)
+  -ESP8266Wifi
+
+  To run this example define the platformio.ini as below.
+
+  [env:nodemcuv2]
+  platform = espressif8266
+  board = nodemcuv2
+  framework = arduino
+  build_flags = -D PIO_FRAMEWORK_ARDUINO_LWIP2_HIGHER_BANDWIDTH
+
+  lib_deps =
+    ESP_VS1053_Library
+
+  Instructions:
+  -Build the hardware
+    (please find an additional description and Fritzing's schematic here:
+     https://github.com/vincegellar/Simple-Radio-Node#wiring)
+  -Set the station in this file
+  -Upload the program
+
+  IDE Settings (Tools):
+  -IwIP Variant: v1.4 Higher Bandwidth
+  -CPU Frequency: 160Hz
+*/
+
+#include <FreeRTOS.h>
 #include <VS1053.h>
-#endif
+#include <WiFi.h>
+#include <WiFiMulti.h>
 
-#if 0
-MFRC522 mfrc522(16 /* SS_PIN */, 17 /* RST_PIN */);
-SdFat sd;
-#else
-VS1053 player(32 /* VS1053_CS */, 33 /* VS1053_DCS */, 34 /* VS1053_DREQ */);
-#endif
+#include <utility>
+
+struct StreamFrame
+{
+  uint8_t buf[64];
+  size_t numBytes;
+};
+
+uint8_t const VS1053_CS = 32;
+uint8_t const VS1053_DCS = 33;
+uint8_t const VS1053_DREQ = 34;
+uint8_t const VOLUME = 100;
+
+//  http://stream-uk1.radioparadise.com:80/mp3-128
+char const* host = "stream-uk1.radioparadise.com";
+char const* path = "/mp3-128";
+int const httpPort = 80;
+
+std::pair<char const*, char const*> const wifiCredentials[] = {
+    // {"SSID", "PASSWORD"},
+};
+
+VS1053 player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
+WiFiClient wifiClient;
+WiFiMulti wifiMulti;
+auto queue = xQueueCreate(8, sizeof(StreamFrame));
+
+static void setupWIFI(void);
+static void TaskHttpReader(void* param);
 
 void setup()
 {
   Serial.begin(115200);
-  while(!Serial)
-  {
-  }
+
+  // Wait for VS1053 and PAM8403 to power up
+  // otherwise the system might not start up correctly
+  delay(3000);
+
+  Serial.println("\n\nSimple Radio Node WiFi Radio");
+
   SPI.begin();
 
-#if 0
-  mfrc522.PCD_Init();
-  sd.begin(0 /* CS_PIN */);
-#else
+  setupWIFI();
+
   player.begin();
   player.switchToMp3Mode();
-  player.setVolume(100 /* VOLUME */);
+  player.setVolume(VOLUME);
 
-#endif
+  xTaskCreate(TaskHttpReader, "HttpReader", 4096 /* Stack size */, nullptr, 2 /* priority */,
+              nullptr);
 }
 
 void loop()
 {
-  static size_t cnt = 0;
-#if 0
-  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
+  StreamFrame frame;
+  xQueueReceive(queue, &frame, portMAX_DELAY);
+  player.playChunk(frame.buf, frame.numBytes);
+}
+
+static void TaskHttpReader(void* param)
+{
+  Serial.print("Connecting ");
+
+  while (wifiMulti.run() != WL_CONNECTED)
   {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  if (!wifiClient.connect(host, httpPort))
+  {
+    Serial.println("Connection failed");
     return;
   }
 
-  Serial.print("New card detected: ");
-  for (byte i = 0; i < mfrc522.uid.size; i++)
+  Serial.print("Requesting stream: ");
+  Serial.println(path);
+
+  wifiClient.print(String("GET ") + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +
+                   "Connection: close\r\n\r\n");
+  for (;;)
   {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    if (!wifiClient.connected())
+    {
+      Serial.println("Reconnecting...");
+      if (wifiClient.connect(host, httpPort))
+      {
+        wifiClient.print(String("GET ") + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +
+                         "Connection: close\r\n\r\n");
+      }
+    }
+    while (wifiClient.available() > 0)
+    {
+      StreamFrame frame;
+      frame.numBytes = wifiClient.read(frame.buf, sizeof(frame.buf));
+      xQueueSend(queue, &frame, portMAX_DELAY);
+    }
   }
-  Serial.println();
-#else
-  player.playChunk(sampleMp3, sizeof(sampleMp3));
-  Serial.print(cnt++);
-  Serial.print(": played chunk of size ");
-  Serial.print(sizeof(sampleMp3));
-  Serial.println(" bytes.");
-  delay(500);
-#endif
+}
+
+static void setupWIFI(void)
+{
+  for (auto credentialPair : wifiCredentials)
+  {
+    wifiMulti.addAP(credentialPair.first, credentialPair.second);
+  }
 }
